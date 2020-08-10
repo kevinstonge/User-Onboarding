dde9f9342a6d948c62ff64810e362a2cd6a266aa
Kevin St.Onge <kevinstonge@gmail.com>
2020-08-10 12:37:07 -0400

    updating error state with Yup

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -4,7 +4,7 @@ import "./Form.scss";
 
 const Form = (props) => {
   const formSchema = Yup.object().shape({
-    name: Yup.string().required(),
+    name: Yup.string().required("please enter your name"),
     email: Yup.string()
       .email("please enter a valid email address")
       .required("an email address is required"),
@@ -17,34 +17,67 @@ const Form = (props) => {
       "you must agree to the terms of service"
     ),
   });
-  const [errors, setErrors] = useState(
-    Object.assign(
-      {},
-      ...Object.keys(formSchema.fields).map((k) => {
-        return { [k]: "" };
-      })
-    )
+  const empty = Object.assign(
+    {},
+    ...Object.keys(formSchema.fields).map((k) => {
+      return { [k]: "" };
+    })
   );
-  console.log(errors);
+  const [errors, setErrors] = useState(empty);
+  const [values, setValues] = useState(empty);
+  const handleInput = (e) => {
+    let { id, value } = e.target;
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
+    }
+    setValues({ ...values, [id]: value });
+    Yup.reach(formSchema, id)
+      .validate(value)
+      .then(() => {
+        setErrors({
+          ...errors,
+          [id]: "",
+        });
+      })
+      .catch((err) => {
+        setErrors({
+          ...errors,
+          [id]: err.errors[0],
+        });
+      });
+  };
   return (
     <form>
-      {Object.keys(formSchema.fields).map((k) => (
-        <div className="formSection" key={`${k}-formSection`}>
-          <div className="formRow">
-            <label htmlFor={k}>{k}</label>
-            {formSchema.fields[k].type === "string" && k !== "password" ? (
-              <input type="text" id={k}></input>
-            ) : null}
-            {k === "password" ? <input type="password" id={k}></input> : null}
-            {formSchema.fields[k].type === "boolean" ? (
-              <input type="checkbox" id={k}></input>
+      <h3>New User Information</h3>
+      {Object.keys(formSchema.fields).map((k) => {
+        let inputType = "text";
+        if (k === "password") {
+          inputType = "password";
+        }
+        if (formSchema.fields[k].type === "boolean") {
+          inputType = "checkbox";
+        }
+
+        return (
+          <div className="formSection" key={`${k}-formSection`}>
+            <div className="formRow">
+              <label htmlFor={k}>{k}</label>
+              <input
+                type={inputType}
+                id={k}
+                value={values[k]}
+                checked={values[k]}
+                onChange={(e) => handleInput(e)}
+              />
+            </div>
+            {errors[k].length > 0 ? (
+              <p className="formError" id={`${k}-error`}>
+                {errors[k]}
+              </p>
             ) : null}
           </div>
-          <p className="formError" id={`${k}-error`}>
-            error text
-          </p>
-        </div>
-      ))}
+        );
+      })}
     </form>
   );
 };
